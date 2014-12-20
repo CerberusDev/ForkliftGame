17f27fbac94ae5863e63173bee001ff425d75fb1
Maciej Pryc <pryc.maciej@gmail.com>
2014-12-20 20:07:47 +0100

    Forklift (vehicle) added + simple movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿// Forklift Game
+//
+// Script author: Maciej Pryc
+// Created: 2014/12/20
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 6.0f;
+
+	Rigidbody2D playerRigidBody;
+	Vector2 inputForce;
+	
+	void Awake () {
+		playerRigidBody = GetComponent<Rigidbody2D> ();
+		inputForce = new Vector2 (0.0f, 0.0f);
+	}
+
+	void FixedUpdate () {
+		inputForce.x = 0.0f;
+		inputForce.y = 0.0f;
+
+		if (Input.GetKey(KeyCode.RightArrow)) 
+		{
+			inputForce.x = 1000.0f;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			inputForce.x -= 1000.0f;
+		}
+
+		playerRigidBody.AddForce (inputForce);
+	}
+}
