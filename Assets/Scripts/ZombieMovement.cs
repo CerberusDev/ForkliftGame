febad40cfbc31c63725be0318403ecd295202ea7
Maciej Pryc <pryc.maciej@gmail.com>
2015-01-03 20:31:03 +0100

    First, simple version of zombie-enemy added

---
+++ Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,31 @@
+﻿// Forklift Game
+//
+// Script author: Maciej Pryc
+// Created: 2015/01/03
+
+using UnityEngine;
+using System.Collections;
+
+public class ZombieMovement : MonoBehaviour {
+	
+	public float speed = 1.0f;
+	
+	Rigidbody2D zombieRigidBody;
+	Vector2 speedVector;
+	//Vector2 gravityVector;
+	
+	void Awake () {
+		zombieRigidBody = GetComponent<Rigidbody2D> ();
+		speedVector = new Vector2 (-speed, 0.0f);
+		//gravityVector = new Vector2 (0.0f, -1.0f);
+	}
+	
+	void FixedUpdate () {
+		//zombieRigidBody.MovePosition (zombieRigidBody.position + speedVector * Time.deltaTime);
+		//zombieRigidBody.AddForce (gravityVector * zombieRigidBody.gravityScale);
+		//zombieRigidBody.AddForce (speedVector);
+		//zombieRigidBody.position += speedVector * Time.deltaTime;
+		//zombieRigidBody.MovePosition (zombieRigidBody.position + (speedVector + gravityVector) * Time.deltaTime);
+		transform.Translate (speedVector * Time.deltaTime);
+	}
+}
