bd8aed384440dd16585c3affa21d4001665eca3d
Maciej Pryc <pryc.maciej@gmail.com>
2015-01-03 23:52:47 +0100

    Movement refactor + fork movement (up / down)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,29 +8,53 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed = 6.0f;
+	public float playerSpeed = 1000.0f;
+	public float forkSpeed = 1.0f;
 
 	Rigidbody2D playerRigidBody;
+	Transform forkTransform;
 	Vector2 inputForce;
+	Vector2 forkMovement;
+	float maxForkHeight;
+	float minForkHeight;
 	
 	void Awake () {
 		playerRigidBody = GetComponent<Rigidbody2D> ();
+		forkTransform = transform.FindChild ("ForkliftFork");
+		maxForkHeight = transform.FindChild ("SocketForkTop").transform.position.y;
+		minForkHeight = transform.FindChild ("SocketForkBottom").transform.position.y;
+
 		inputForce = new Vector2 (0.0f, 0.0f);
+		forkMovement = new Vector2 (0.0f, 0.0f);
 	}
 
 	void FixedUpdate () {
 		inputForce.x = 0.0f;
 		inputForce.y = 0.0f;
 
+		forkMovement.x = 0.0f;
+		forkMovement.y = 0.0f;
+
+		float currForkHeight = forkTransform.position.y;
+
 		if (Input.GetKey(KeyCode.RightArrow)) 
 		{
-			inputForce.x = 1000.0f;
+			inputForce.x = playerSpeed;
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) 
 		{
-			inputForce.x -= 1000.0f;
+			inputForce.x -= playerSpeed;
+		}
+		if (Input.GetKey(KeyCode.UpArrow) && currForkHeight < maxForkHeight) 
+		{
+			forkMovement.y = forkSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow) && currForkHeight > minForkHeight) 
+		{
+			forkMovement.y -= forkSpeed * Time.deltaTime;
 		}
 
+		forkTransform.Translate(forkMovement);
 		playerRigidBody.AddForce (inputForce);
 	}
 }
--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -10,12 +10,12 @@ public class ZombieMovement : MonoBehaviour {
 	
 	public float speed = 1.0f;
 	
-	Rigidbody2D zombieRigidBody;
+	Transform zombieTransform;
 	Vector2 speedVector;
 	//Vector2 gravityVector;
 	
 	void Awake () {
-		zombieRigidBody = GetComponent<Rigidbody2D> ();
+		zombieTransform = GetComponent<Transform> ();
 		speedVector = new Vector2 (-speed, 0.0f);
 		//gravityVector = new Vector2 (0.0f, -1.0f);
 	}
@@ -26,6 +26,6 @@ public class ZombieMovement : MonoBehaviour {
 		//zombieRigidBody.AddForce (speedVector);
 		//zombieRigidBody.position += speedVector * Time.deltaTime;
 		//zombieRigidBody.MovePosition (zombieRigidBody.position + (speedVector + gravityVector) * Time.deltaTime);
-		transform.Translate (speedVector * Time.deltaTime);
+		zombieTransform.Translate (speedVector * Time.deltaTime);
 	}
 }
