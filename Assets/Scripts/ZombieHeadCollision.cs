fdb62eda7f19f636254b5802c70523bf2c97aa7a
Maciej Pryc <pryc.maciej@gmail.com>
2015-01-04 20:21:36 +0100

    Setup for advanced colision detection on Zombie

---
+++ Assets/Scripts/ZombieHeadCollision.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieHeadCollision : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log (coll.gameObject);
+	}
+}
