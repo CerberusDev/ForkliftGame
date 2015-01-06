c1f24c225bc374878ab1b2f30c1336f493907cb4
Maciej Pryc <pryc.maciej@gmail.com>
2015-01-06 22:33:55 +0100

    Delayed destroy ZombieHead objects

---
+++ Assets/Scripts/DelayedDestroy.cs
@@ -0,0 +1,16 @@
+﻿// Forklift Game
+//
+// Script author: Maciej Pryc
+// Created: 2015/01/06
+
+using UnityEngine;
+using System.Collections;
+
+public class DelayedDestroy : MonoBehaviour {
+
+	public float destroyDelay = 10.0f;
+
+	void Start () {
+		Destroy (gameObject, destroyDelay);
+	}
+}
