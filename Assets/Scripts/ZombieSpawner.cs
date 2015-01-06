9d222781380514f8798be1906c1031dbbea89144
Maciej Pryc <pryc.maciej@gmail.com>
2015-01-06 17:23:45 +0100

    Zombie spawner added

---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSpawner : MonoBehaviour {
+
+	public float spawnInterval = 3.0f;
+	public GameObject[] zombiePrefabs;
+
+	Transform spawnPoint;
+	float timer;
+
+	void Start () {
+		spawnPoint = transform;
+		timer = 0.0f;
+	}
+
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0.0f) {
+			SpawnZombie();
+			timer += spawnInterval;
+		}
+	}
+
+	void SpawnZombie() {
+		int randomZombiePrefabIndex = Random.Range(0, zombiePrefabs.Length);
+		GameObject randomZombiePrefab = zombiePrefabs [randomZombiePrefabIndex];
+
+		Instantiate (randomZombiePrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
