afc3955951d42aaa89156909767db9f839e353c3
Maciej Pryc <pryc.maciej@gmail.com>
2015-01-09 10:00:28 +0100

    Kill volume added

---
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,15 @@
+﻿// Forklift Game
+//
+// Script author: Maciej Pryc
+// Created: 2015/01/09
+
+using UnityEngine;
+using System.Collections;
+
+public class DestroyOnCollision : MonoBehaviour {
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log (coll.gameObject);
+		Destroy (coll.gameObject);
+	}
+}
