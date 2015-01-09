5cbabec102db692cdf56960197e2cc698ee9a6f9
Maciej Pryc <pryc.maciej@gmail.com>
2015-01-09 10:30:59 +0100

    Camera interpolation (attached to the forklift)

---
+++ Assets/Scripts/SmoothAttach.cs
@@ -0,0 +1,32 @@
+﻿// Forklift Game
+//
+// Script author: Maciej Pryc
+// Created: 2015/01/09
+
+using UnityEngine;
+using System.Collections;
+
+public class SmoothAttach : MonoBehaviour {
+
+	public GameObject attachBase;
+
+	Transform baseTransform;
+	Transform myTransform;
+	Vector3 offset;
+
+	void Start() {
+		myTransform = transform;
+		baseTransform = attachBase.transform;
+		offset = new Vector3 (4.0f, 0.0f, -10.0f);
+	}
+
+	void Update () {
+		myTransform.Translate (-offset);
+		Vector3 diff = baseTransform.position - myTransform.position;
+
+		diff /= 8.0f;
+
+		myTransform.position = myTransform.position + diff;
+		myTransform.Translate (offset);
+	}
+}
