59a838cbc0f12c6512c830ebe2b08b52f16f1d82
Kwiatmen <roleman@poczta.onet.pl>
2015-01-23 13:34:39 +0100

    Start  menu, + background + holder for instructions and stats menu

---
+++ Assets/Scripts/Interfaces/MenuScript.cs
@@ -0,0 +1,88 @@
+﻿// Forklift Game
+//
+// Script author: Alan Kwiatkowski
+// Created: 2015/01/23
+using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour 
+{
+	public Canvas Menu_start;
+	public Canvas Menu_instructions;
+	public Canvas Menu_stats;
+	public Canvas Background;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		SetEnable(true);
+	}
+
+	public void SetEnable( bool inState )
+	{
+		enabled = inState;
+		Background.enabled = inState;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	bool IsStartMenuVisible()
+	{
+		return Menu_start.enabled;
+	}
+
+	bool IsInstructionsMenuVisible()
+	{
+		return Menu_instructions.enabled;
+	}
+
+	bool IsStatsMenuVisible()
+	{
+		return Menu_stats.enabled;
+	}
+
+	public void Menu_startPressed( int action )
+	{
+		switch (action)
+		{
+		case 0:
+			print ("Start");
+
+			SetEnable(false);
+			Menu_start.enabled = false;
+			break;
+		case 1:
+			print ("Instructions");
+
+			break;
+		case 2:
+			print ("QUIT");
+
+			Application.Quit();
+			break;
+		}
+	}
+
+	public void Menu_instructionsPressed( int action )
+	{
+		switch (action)
+		{
+		case 0:
+			break;
+		}
+	}
+
+	public void Menu_statsPressed( int action )
+	{
+		switch (action)
+		{
+		case 0:
+					
+			break;
+		}
+	}
+}
